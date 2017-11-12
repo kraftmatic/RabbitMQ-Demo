ca6e30201eeb5de608ec56970462042b5c564ce5
Nicholas Kraftor <nkraftor@skytouchtechnology.com>
2017-11-11 23:04:57 -0700

    Refactor core app to be a blocking-subscriber for perpetual processing

--- clojure-client/project.clj
+++ clojure-client/project.clj
@@ -1,10 +1,10 @@
-(defproject clojure-noob "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+(defproject clojure-client "0.1.0-SNAPSHOT"
+  :description "A lightweight clojure application with RabbitMQ implementation via Longohr"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"],
                  [com.novemberain/langohr "3.6.1"]]
-  :main ^:skip-aot clojure-noob.core
+  :main clojure-client.core
+  :source-paths ["src" "src/clojure_client"]
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
